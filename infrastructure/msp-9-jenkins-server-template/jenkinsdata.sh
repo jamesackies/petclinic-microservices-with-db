3eef2224ea5c3a64321d8b4c1957580b7424dc99
jamesackies <jamesackies8@gmail.com>
2026-01-23 01:09:02 +0000

    updated userdata

--- infrastructure/msp-9-jenkins-server-template/jenkinsdata.sh
+++ infrastructure/msp-9-jenkins-server-template/jenkinsdata.sh
@@ -6,13 +6,13 @@ hostnamectl set-hostname jenkins-server
 # install git
 dnf install git -y
 # install java 11
-dnf install fontconfig java-11-amazon-corretto -y
+dnf install fontconfig java-17-amazon-corretto-devel -y
 
 # install jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+rpm --import  https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 dnf upgrade -y
-dnf install jenkins-2.452.3-1.1 -y
+dnf install jenkins -y
 
 # install docker
 dnf install docker -y
